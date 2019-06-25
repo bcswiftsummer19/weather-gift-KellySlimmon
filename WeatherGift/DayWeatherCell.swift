06e223f8ffd633737f342709ceff1c0cd7ecaf61
Kelly Slimmon <slimmon@bc.edu>
2019-06-25 22:56:21 +0100

    Final App

---
+++ WeatherGift/DayWeatherCell.swift
@@ -0,0 +1,46 @@
+//
+//  DayWeatherCell.swift
+//  WeatherGift
+//
+//  Created by Kelly Slimmon on 6/23/19.
+//  Copyright © 2019 Kelly Slimmon. All rights reserved.
+//
+
+import UIKit
+
+private let dateFormatter: DateFormatter = {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "EEEE"
+    return dateFormatter
+}()
+
+class DayWeatherCell: UITableViewCell {
+    @IBOutlet weak var dayCellIcon: UIImageView!
+    @IBOutlet weak var dayCellWeekday: UILabel!
+    @IBOutlet weak var dayCellMaxTemp: UILabel!
+    @IBOutlet weak var dayCellMinTemp: UILabel!
+    @IBOutlet weak var dayCellSummary: UITextView!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+    func update(with dailyForecast: WeatherLocation.DailyForecast, timeZone: String) {
+        dayCellIcon.image = UIImage(named: dailyForecast.dailyIcon)
+        dayCellSummary.text = dailyForecast.dailySummary
+        dayCellMaxTemp.text = String(format: "%2.f", dailyForecast.dailyMaxTemp) + "°"
+        dayCellMinTemp.text = String(format: "%2.f", dailyForecast.dailyMinTemp) + "°"
+
+        let dateString = dailyForecast.dailyDate.format(timeZone: timeZone, dateFormatter: dateFormatter)
+        
+        dayCellWeekday.text = dateString
+    }
+
+}
--- WeatherGift/DetailVC.swift
+++ WeatherGift/DetailVC.swift
@@ -9,25 +9,39 @@
 import UIKit
 import CoreLocation
 
+private let dateFormatter: DateFormatter = {
+    let dateFormatter = DateFormatter()
+    return dateFormatter
+}()
+
+
 class DetailVC: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var summaryLabel: UILabel!
     @IBOutlet weak var currentImage: UIImageView!
+    @IBOutlet weak var collectionView: UICollectionView!
     
     var currentPage = 0
     var locationsArray = [WeatherLocation]()
     var locationManager: CLLocationManager!
     var currentLocation: CLLocation!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        collectionView.delegate = self
+        collectionView.dataSource = self
         if currentPage != 0 {
-            self.locationsArray[0].getWeather {
+            self.locationsArray[currentPage].getWeather {
                 self.updateUserInterface()
             }
         }
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -38,12 +52,17 @@ class DetailVC: UIViewController {
     }
     
     func updateUserInterface(){
-        locationLabel.text = locationsArray[currentPage].name
-        dateLabel.text = locationsArray[currentPage].coordinates
-        temperatureLabel.text = locationsArray[currentPage].currentTemp
-        currentImage.image = UIImage(named: locationsArray[currentPage].currentIcon)
-        summaryLabel.text = locationsArray[currentPage].currentSummary
+        let location = locationsArray[currentPage]
+        locationLabel.text = location.name
+        let dateString = location.currentTime.format(timeZone: location.timeZone, dateFormatter: dateFormatter)
+        dateLabel.text = dateString
+        temperatureLabel.text = location.currentTemp
+        currentImage.image = UIImage(named: location.currentIcon)
+        summaryLabel.text = location.currentSummary
+        tableView.reloadData()
+        collectionView.reloadData()
     }
+    
 }
 
 extension DetailVC: CLLocationManagerDelegate {
@@ -73,7 +92,6 @@ extension DetailVC: CLLocationManagerDelegate {
         let currentLatitude = currentLocation.coordinate.latitude
         let currentLongitude = currentLocation.coordinate.longitude
         let currentCoordinates = "\(currentLatitude),\(currentLongitude)"
-        dateLabel.text = currentCoordinates
         geoCoder.reverseGeocodeLocation(currentLocation, completionHandler: {placemarks, error in
             if placemarks != nil{
                 let placemark = placemarks?.last
@@ -89,7 +107,41 @@ extension DetailVC: CLLocationManagerDelegate {
             }
         })
     }
+    
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("Failed to get user location.")
+    }
+}
+
+
+extension DetailVC: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return locationsArray[currentPage].dailyForecastArray.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DayWeatherCell", for: indexPath) as! DayWeatherCell
+        let dailyForecast = (locationsArray[currentPage].dailyForecastArray[indexPath.row])
+        let timeZone = locationsArray[currentPage].timeZone
+        cell.update(with: dailyForecast, timeZone: timeZone)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+}
+
+extension DetailVC: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return locationsArray[currentPage].hourlyForecastArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let hourlyCell = collectionView.dequeueReusableCell(withReuseIdentifier: "HourlyCell", for: indexPath) as! HourlyWeatherCell
+        let hourlyForecast = locationsArray[currentPage].hourlyForecastArray[indexPath.row]
+        let timeZone = locationsArray[currentPage].timeZone
+        hourlyCell.update(with: hourlyForecast, timeZone: timeZone)
+        return hourlyCell
     }
 }
